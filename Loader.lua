04a3335009a431603f1494f6e42b5b74ac00178c
Stormscripts <deborayanlopes21@gmail.com>
2026-03-22 20:46:55 -0300

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,29 @@
+local PlaceId = game.PlaceId
+
+if PlaceId == 131623223084840 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/Escape%20Tsunami%20For%20Brainrots%20Storm%20Hub%20stormscripts"))()
+
+elseif PlaceId == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/MM2%20Storm%20Hub%20stormscripts"))()
+
+elseif PlaceId == 3082002798 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/T-Titans%20Battlegrounds%20Storm%20Hub%20stormscripts"))()
+
+elseif PlaceId == 16560655288 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/Pillar%20Chase%202%20Storm%20Hub%20stormscripts"))()
+
+elseif PlaceId == 109629391874197 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/Marvel%20Omega%20Storm%20Hub%20stormscripts"))()
+
+elseif PlaceId == 15131312477 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/Ultimate%20Soccer%20Storm%20Hub%20stormscripts"))()
+
+elseif PlaceId == 893973440 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/Flee%20The%20Facility%20Storm%20Hub%20stormscripts"))()
+
+elseif PlaceId == 9224601490 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stormscripts/Storm-Hub-Loader/refs/heads/main/Fruit%20Battlegrounds%20Storm%20Hub%20stormscripts"))()
+
+else
+    game:GetService("Players").LocalPlayer:Kick("⚠️ Storm Hub | Jogo não suportado!")
+end
